5eec2bbc70c341cb99054d33e4f14a6509191243
Eddie M. Loeza <loezaeddie@gmail.com>
2016-10-07 11:27:55 -0700

    my project workd

---
+++ drawingproject.js
@@ -0,0 +1,14 @@
+var canvasDiv = document.getElementById("drawingArea");
+var canvas = canvasDiv.getContext('2d');
+
+canvas.fillStyle = "Red";
+canvas.fillRect(300, 200, 30, 30);
+canvas.fillRect(300, 230, 30, 30);
+canvas.fillRect(300, 260, 30, 30);
+canvas.fillRect(300, 290, 30, 30);
+canvas.fillRect(300, 300, 30, 30);
+canvas.fillRect(330, 200, 30, 30);
+canvas.fillRect(330, 230, 30, 30);
+canvas.fillRect(330, 260, 30, 30);
+canvas.fillRect(330, 290, 30, 30);
+canvas.fillRect(330, 300, 30, 30);
